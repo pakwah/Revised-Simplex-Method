87f0a4c2e9fda0e6145c694a3bec78f4be04d04e
pakwah <xuanbaihua1@gmail.com>
2015-10-18 23:51:16 -0500

    Initial Commit

---
+++ RevisedSimplex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RevisedSimplex
+//
+//  Created by Xuan Baihua on 10/18/15.
+//  Copyright (c) 2015 Xuan Baihua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
